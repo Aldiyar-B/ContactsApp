2d5576d2689e9bddb328488cde2026b3c84f9ca8
Aldiyar-B <aimamadiev@mail.ru>
2024-04-19 21:51:00 +0700

    Разработана логика приложения

--- ContactsAppUI/ContactsAppUI/Form1.Designer.cs
+++ ContactsAppUI/ContactsAppUI/Form1.Designer.cs
@@ -28,19 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtVkId = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
+            // 
+            // txtVkId
+            // 
+            txtVkId.Location = new Point(88, 83);
+            txtVkId.Name = "txtVkId";
+            txtVkId.Size = new Size(125, 27);
+            txtVkId.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(512, 81);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(512, 164);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(txtVkId);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtVkId;
+        private Button button1;
+        private Button button2;
     }
 }
--- ContactsAppUI/ContactsAppUI/Form1.cs
+++ ContactsAppUI/ContactsAppUI/Form1.cs
@@ -13,5 +13,43 @@ namespace ContactsAppUI
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string vkId = txtVkId.Text; // Получение данных из TextBox
+
+            if (!string.IsNullOrEmpty(vkId))
+            {
+                Contact contact = new Contact();
+                contact.VkID = vkId;
+
+                // Сохранение данных в файл
+                List<Contact> contacts = new List<Contact>();
+                contacts.Add(contact);
+                Serializer.SaveToFile(contacts, "contacts.json");
+
+                MessageBox.Show("Данные успешно сериализованы и сохранены в файл contacts.json");
+            }
+            else
+            {
+                MessageBox.Show("Введите VkID в TextBox");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Загрузка данных из файла
+            List<Contact> loadedContacts = Serializer.LoadFromFile("contacts.json");
+
+            if (loadedContacts.Count > 0)
+            {
+                Contact loadedContact = loadedContacts[0];
+                MessageBox.Show($"VkID из загруженного объекта: {loadedContact.VkID}");
+            }
+            else
+            {
+                MessageBox.Show("Нет данных для десериализации");
+            }
+        }
     }
 }
